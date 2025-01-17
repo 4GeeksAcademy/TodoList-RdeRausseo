9023ee4618daab921550e0ad81b63cd54a9eb037
RdeRausseo <amrausseo20@gmail.com>
2025-01-17 23:34:44 +0000

    tarea finalizada, se puede borrar y agregar tareas, hover de boton funcional tambien

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,48 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [list, setList] = useState([])
+
+	const deleteTask = (id) => {
+		setList(list.filter(task => task.id !== id))
+	}
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<h1 className="display-1 mt-5 text-primary-emphasis text-center">Todos</h1>
+			<div className="container bg-primary-subtle">
+				<div className="row p-4">
+					<div className="col-12 mb-3">
+						<input type="text" className="form-control" placeholder="What need to be done ?" onKeyDown={e => { e.key === "Enter" && e.target.value !== "" ? setList([...list, { inf: e.target.value, id: list.length + 1 }]) : "" }} />
+					</div>
+
+					<ul className="list-group bg-light ps-2">
+						{
+							list.map((item) => {
+								return (
+									<>
+										<li className="list-group" key={item.id}>
+											<div className="d-flex justify-content-between p-3">
+												<p className="fs-3"> {item.inf} </p>
+												<button onClick={() => deleteTask(item.id)} className="btn btn-danger show" > <span> X </span> </button>
+											</div>
+										</li>
+										<hr />
+									</>
+
+								)
+							})
+						}
+					</ul>
+					<span className="text-secondary mt-2"> {list.length} item left </span>
+
+				</div>
+			</div>
+		</>
 	);
+
 };
 
 export default Home;
